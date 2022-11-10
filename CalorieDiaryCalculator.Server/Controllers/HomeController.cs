89ae529b7c5dd214c1a75fdf4a9a68fcd06c4895
dimitar.yotov <dimitar.yotov@blu11.com>
2022-11-10 19:55:20 +0200

    HomeController -> IActionResult Get()

--- CalorieDiaryCalculator.Server/Controllers/HomeController.cs
+++ CalorieDiaryCalculator.Server/Controllers/HomeController.cs
@@ -1,7 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CalorieDiaryCalculator.Server.Controllers {
     [ApiController]
     public class HomeController : ControllerBase {
+        [Authorize]
+        public IActionResult Get() {
+            return this.Ok("Works!");
+        }
     }
 }
